5ac8a968bbfa503a187c9de160b0e56a2442610c
Florian G <florian.grundmann@freenet.de>
2020-05-11 16:34:52 +0700

    Added MonsterRow.

--- lib/features/monster_cards/presentation/widgets/monster_row.dart
+++ lib/features/monster_cards/presentation/widgets/monster_row.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:monster_cards/core/styles/foxling_colors.dart';
-import 'package:monster_cards/features/monster_cards/domain/entities/monster.dart';
-import 'package:monster_cards/features/monster_cards/presentation/widgets/home_body.dart';
+
+import '../../../../core/styles/foxling_colors.dart';
+import '../../../../core/styles/poppins_text_style.dart';
+import '../../domain/entities/monster.dart';
+import '../../domain/entities/monster_card_stats.dart';
+import 'home_body.dart';
 
 class MonsterRow extends StatelessWidget {
-  final Monster monster;
+  final MonsterCardStats monster;
 
   const MonsterRow({
     Key key,
@@ -62,6 +65,93 @@ class MonsterCard extends StatelessWidget {
           ),
         ],
       ),
+      child: MonsterCardContent(monster: monster),
     );
+  }
+}
+
+class MonsterCardContent extends StatelessWidget {
+  final Monster monster;
+
+  const MonsterCardContent({Key key, this.monster}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(40.0, 8.0, 8.0, 8.0),
+      child: Column(
+        children: <Widget>[
+          Header(monster.name),
+          Seperator(),
+          ImportantStats(),
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  final String headerText;
+
+  const Header(this.headerText, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(headerText, style: PoppinsTextStyle.headerTextStyle);
+  }
+}
+
+class Seperator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0),
+      height: 2.0,
+      width: 50.0,
+      color: FoxlingColors.orange,
+    );
+  }
+}
+
+class ImportantStats extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              ImportantStatText('Fight.: ${monster.fighting.value}'),
+              ImportantStatText('Dmg: ${monster.dmg}'),
+              ImportantStatText('Notice: ${monster.notice.value}'),
+            ],
+          ),
+        ),
+        SizedBox(width: 20),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ImportantStatText('Parry: ${monster.parry}'),
+              ImportantStatText('Tough.: ${monster.toughness}'),
+              ImportantStatText('Pace: ${monster.pace}'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ImportantStatText extends StatelessWidget {
+  final String text;
+
+  const ImportantStatText(this.text, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: PoppinsTextStyle.subHeaderTextStyle);
   }
 }
